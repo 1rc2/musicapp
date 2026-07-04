585817157e4412aebf53c3788f99d256fb71d5ab
1rc2 <1rc2@users.noreply.github.com>
2026-07-05 00:08:50 +0800

    修复Maven仓库配置，使用标准google/mavenCentral

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,8 +1,6 @@
 allprojects {
     repositories {
-        maven { url = uri("file:///opt/local-maven-repo") }
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        google()
         mavenCentral()
     }
 }
--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,8 +1,6 @@
 buildscript {
     repositories {
-        maven { url = uri("file:///opt/local-maven-repo") }
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        google()
         mavenCentral()
     }
     dependencies {
